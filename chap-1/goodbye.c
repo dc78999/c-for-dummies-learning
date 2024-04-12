b0f2ff03a8e1de687bb2c102de293788a907ad1d
dc78999 <c.dinh@uqconnect.edu.au>
2024-04-12 21:20:53 +1000

    adding some spaces to goodbye.c to comply with the spelling checker

--- chap-1/goodbye.c
+++ chap-1/goodbye.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-	printf("%s","Goodbye,Cruel world!\n");
+	printf("%s","Goodbye, Cruel world!\n");
 	printf("Goodbye, Cruel world!\n");
-	printf("%s,%s %s\n","Goodbye", "Cruel", "world!");
+	printf("%s, %s %s\n","Goodbye", "Cruel", "world!");
 	return 0;}
